eaa606485c7e1af4db2927f52d8ca3c3b79e6cdb
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-02 12:50:39 -0500

    update course index method

--- Interactive_Classroom/app/controllers/courses_controller.rb
+++ Interactive_Classroom/app/controllers/courses_controller.rb
@@ -1,5 +1,8 @@
 class CoursesController < ApplicationController
   def index
+    #IF THIS IS NOT ADMIN eventually change to @courses = Course.all.where(:user_id => current_user.id)
+      #keep how it is for now to see full list of courses, make admin namespace that will have Course.all
+@courses = Course.all
   end
 
   def create
