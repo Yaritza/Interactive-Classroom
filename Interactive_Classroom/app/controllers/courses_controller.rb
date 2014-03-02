c740626feacbeab035ef36eda9b2f1641c3641cb
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-02 13:02:00 -0500

    update create method in courses

--- Interactive_Classroom/app/controllers/courses_controller.rb
+++ Interactive_Classroom/app/controllers/courses_controller.rb
@@ -2,10 +2,21 @@ class CoursesController < ApplicationController
   def index
     #IF THIS IS NOT ADMIN eventually change to @courses = Course.all.where(:user_id => current_user.id)
       #keep how it is for now to see full list of courses, make admin namespace that will have Course.all
-@courses = Course.all
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
   end
 
   def create
+    course = Course.create safe_course
+    redirect_to course
+  end
 
+  private
+
+  def safe_course
+    params.require(:course).permit(:name, :number)
   end
 end
