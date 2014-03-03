36ab0200e391c5dca07a1b8a949fe8f1cd474502
Yaritza <saturn540@gmail.com>
2014-03-02 22:30:50 -0500

    Do not merge with master

---
+++ Interactive_Classroom/db/migrate/20140302214836_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.belongs_to :user, index: true
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
--- Interactive_Classroom/db/schema.rb
+++ Interactive_Classroom/db/schema.rb
@@ -11,11 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140223180856) do
+ActiveRecord::Schema.define(version: 20140302214836) do
+
+  create_table "answers", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "courses", force: true do |t|
     t.string   "name"
     t.integer  "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "questions", force: true do |t|
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.integer  "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
